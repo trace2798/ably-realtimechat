c164f44516c4892a19999d5f60ae45084987d867
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-09-03 22:27:40 +0530

    fix for one build error

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <main className="min-h-screen w-full flex-col items-center justify-center flex px-5">
-        <h1>Ably's Pub Sub Next.js App Directory implementation</h1>
+        <h1>Ably&apos;s Pub Sub Next.js App Directory implementation</h1>
         <div className="flex mt-10 w-1/2 justify-evenly">
           <Button onClick={() => router.push("/client")}>Client</Button>
           <Button onClick={() => router.push("/server")}>Server</Button>
